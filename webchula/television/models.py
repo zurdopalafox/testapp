725adcbf80ca9ee74c1e1bf2f34308667fee8101
“zurdopalafox” <carlos.palafox.becerril@gmail.com>
2026-02-26 16:28:16 +0100

    xxxx

--- webchula/television/models.py
+++ webchula/television/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import oracledb
+import pymysql
 
 # Create your models here.
 
@@ -17,7 +18,7 @@ class Personaje:
         self.imagen = ""
         self.idSerie = 0
 
-class ServiceSeries:
+class ServiceSeriesOracle:
     def __init__(self):
         self.connection = oracledb.connect(user="system", password="oracle", dsn="localhost/freepdb1")
 
@@ -57,5 +58,48 @@ class ServiceSeries:
         cursor.execute(sql, (id, nombre, imagen, idserie,))
         self.connection.commit()
         cursor.close()    
+
+
+class ServiceSeries:
+    def __init__(self):
+        self.connection = pymysql.connect(host="sql7.freesqldatabase.com", port=3306, user="sql7818176", password="FzMNGLTjrq", database="sql7818176")
+
+    def getSeries(self):
+        cursor = self.connection.cursor()
+        sql = "select * from SERIES"
+        listaseries = [] 
+        cursor.execute(sql)
+        for fila in cursor:
+            serie = Serie()
+            serie.idSerie = fila[0]
+            serie.nombre = fila[1]
+            serie.imagen =fila[2]
+            serie.anyo = fila[3]
+            listaseries.append(serie)
+        cursor.close()
+        return listaseries
+    
+    def getPersonajesSerie(self, idserie):
+        cursor = self.connection.cursor()
+        sql = "select * from PERSONAJES where IDSERIE = %s"
+        listapersonajes = [] 
+        cursor.execute(sql,(idserie,))
+        for fila in cursor:
+            personaje = Personaje()
+            personaje.idPersonaje = fila[0]
+            personaje.personaje = fila[1]
+            personaje.imagen = fila[2]
+            personaje.idSerie = fila[3]
+            listapersonajes.append(personaje)
+        cursor.close()
+        return listapersonajes
+    
+    def insertarPersonaje(self, id, nombre, imagen, idserie):
+        cursor = self.connection.cursor()
+        sql = "insert into PERSONAJES values (%s, %s, %s, %s)"
+        cursor.execute(sql, (id, nombre, imagen, idserie,))
+        self.connection.commit()
+        cursor.close()    
+
 
 
